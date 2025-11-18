3d3d24a8a4efdba996cc1ec1a16a7a44c47e2fc4
nika767 <nikolozzarnadze37@gmail.com>
2025-11-18 20:57:32 +0400

    1

---
+++ level 41/classwork/cw.py
@@ -0,0 +1,16 @@
+#1)დაამატეთ სიას იანვარი
+LIST=[980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+LIST.append("ianvari")
+print("LIST")
+#2)2 ინდექსზე დაამატე ბატი
+sia=[980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+sia.insert(2 , "bati" )
+print(sia)
+#3)5 ინდექსი ამოშალე
+list=[980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+list.pop(5)
+print(list)
+#4) true და kote ამოშალე
+saxelebi=[980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+saxelebi.remove("true")
+saxelebi.remove("kote")
